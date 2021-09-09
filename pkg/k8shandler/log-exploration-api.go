9ff7b5e9a0b583c01ef3c4e6078f0ed70aad667b
Jenny Zhu <jenny.a.zhu@gmail.com>
2021-09-09 15:48:24 -0400

    Creation of servicemonitor for log exploration api integration. Service for log exploration api was also updated to override the label.

--- pkg/k8shandler/log-exploration-api.go
+++ pkg/k8shandler/log-exploration-api.go
@@ -1,6 +1,8 @@
 package k8shandler
 
 import (
+	"github.com/ViaQ/logerr/log"
+	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
 	route "github.com/openshift/api/route/v1"
 	"github.com/openshift/cluster-logging-operator/pkg/factory"
 	"github.com/openshift/cluster-logging-operator/pkg/utils"
@@ -11,7 +13,85 @@ import (
 
 const (
 	logAPIPort = 8080
+	logAPIName = "logexplorationapi"
+	logAPIMetricsPortName = "prom-metrics"
 )
+
+func (clusterRequest *ClusterLoggingRequest)  createLogExplorationAPIServiceMonitor() error {
+	log.Info("hello from createLogExplorationAPIServiceMonitor()10")
+
+	cluster := clusterRequest.Cluster
+
+	desired := NewServiceMonitor("logexplorationapi-service-monitor", cluster.Namespace)
+
+	endpoint := monitoringv1.Endpoint{
+		Port:   logAPIMetricsPortName,
+		Path:   "/metrics",
+		Scheme: "http",
+		//Scheme: "https",
+		//TLSConfig: &monitoringv1.TLSConfig{
+		//	CAFile:     prometheusCAFile,
+		//	ServerName: fmt.Sprintf("%s.%s.svc", logAPIName, cluster.Namespace),
+		//	//ServerName can be e.g. logexplorationapi.openshift-logging.svc
+		//},
+	}
+
+	// JZ: metrics exporter is not included becuase those are use to funnel
+	// data from third party systems as Promthesus metrics
+
+	// JZ : match the label given to deployment
+	labelSelector := metav1.LabelSelector{
+		MatchLabels: map[string]string{
+			"app": logAPIName,
+		},
+	}
+
+	// JZ : pulling all the pieces together to make one servicemonitor object
+	desired.Spec = monitoringv1.ServiceMonitorSpec{
+		JobLabel:  "monitor-logexplorationapi",
+		Endpoints: []monitoringv1.Endpoint{endpoint},
+		Selector:  labelSelector,
+		NamespaceSelector: monitoringv1.NamespaceSelector{
+			MatchNames: []string{cluster.Namespace},
+		},
+	}
+
+	utils.AddOwnerRefToObject(desired, utils.AsOwner(cluster))
+
+	err := clusterRequest.Create(desired)
+	if err != nil {
+		return err
+
+	//if err != nil {
+	//	if !errors.IsAlreadyExists(err) {
+	//		return fmt.Errorf("Failure creating the log-exploration-api ServiceMonitor : %v", err)
+	//	}
+	//	current := &monitoringv1.ServiceMonitor{}
+	//	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+	//		if err = clusterRequest.Get(desired.Name, current); err != nil {
+	//			if errors.IsNotFound(err) {
+	//				// the object doesn't exist -- it was likely culled
+	//				// recreate it on the next time through if necessary
+	//				return nil
+	//			}
+	//			return fmt.Errorf("Failed to get %q service for %q: %v", current.Name, clusterRequest.Cluster.Name, err)
+	//		}
+	//		if servicemonitor.AreSame(current, desired) {
+	//			log.V(3).Info("ServiceMonitor are the same skipping update")
+	//			return nil
+	//		}
+	//		current.Labels = desired.Labels
+	//		current.Spec = desired.Spec
+	//		current.Annotations = desired.Annotations
+	//
+	//		return clusterRequest.Update(current)
+	//	})
+	//	log.V(3).Error(retryErr, "Reconcile ServiceMonitor retry error")
+	//	return retryErr
+	}
+	return nil
+}
+
 
 func newLogExplorationAPIRoute(routeName, namespace, serviceName, componentName, loggingComponent string) *route.Route {
 	return &route.Route{
@@ -45,9 +125,15 @@ func (clusterRequest *ClusterLoggingRequest) createLogExplorationAPIService() er
 			{
 				Port:       logAPIPort,
 				TargetPort: intstr.FromInt(logAPIPort),
+				Name: logAPIMetricsPortName,
 			},
 		},
 	)
+
+	// JZ: Override the Label from factory.NewService
+	desired.Labels = map[string]string{
+		"app" : logAPIName,
+	}
 
 	utils.AddOwnerRefToObject(desired, utils.AsOwner(clusterRequest.Cluster))
 	err := clusterRequest.Create(desired)
@@ -150,31 +236,56 @@ func newLogApiPodSpec(containers []v1.Container, volumes []v1.Volume) v1.PodSpec
 	}
 }
 
+// JZ: remove the print statements
 func (clusterRequest *ClusterLoggingRequest) CreateOrDeleteLogExplorationApi() error {
+	log.Info("CreateOrDeleteLogExplorationAPI entered")
 	if _, ok := clusterRequest.Cluster.Annotations["api-enabled"]; ok {
+		log.Info("api-enabled is TRUE")
 
 		if err := clusterRequest.createLogExplorationAPIDeployment(); err != nil {
-			return err
+			log.Info("ERROR: CreateOrDeleteLogExplorationAPI > Deployment")
+			//return err
 		}
-
 		if err := clusterRequest.createLogExplorationAPIService(); err != nil {
-			return err
+			log.Info("ERROR: CreateOrDeleteLogExplorationAPI > Service")
+
+			//return err
 		}
 		if err := clusterRequest.createLogExplorationAPIRoute(); err != nil {
-			return err
+			log.Info("ERROR: CreateOrDeleteLogExplorationAPI > Route")
+
+			//return err
+		}
+		if err := clusterRequest.createLogExplorationAPIServiceMonitor(); err != nil {
+			log.Info("ERROR: CreateOrDeleteLogExplorationAPI > ServiceMonitor")
+
+			//return err
 		}
 
 	} else {
+		log.Info("api-enabled is FALSE")
 
 		if err := clusterRequest.RemoveDeployment("logging-api"); err != nil {
+			log.Info("ERROR: CreateOrDeleteLogExplorationAPI > RemoveDeployment")
+
 			return err
 		}
 		if err := clusterRequest.RemoveService("logexplorationapi-service"); err != nil {
+			log.Info("ERROR: CreateOrDeleteLogExplorationAPI > RemoveService")
+
 			return err
 		}
 		if err := clusterRequest.RemoveRoute("logexplorationapi-route"); err != nil {
+			log.Info("ERROR: CreateOrDeleteLogExplorationAPI > RemoveRoute")
+
 			return err
 		}
+
+		if err := clusterRequest.RemoveServiceMonitor("logexplorationapi-service-monitor"); err != nil {
+			log.Info("ERROR: CreateOrDeleteLogExplorationAPI > RemoveServiceMonitor")
+			return err
+		}
+
 	}
 	return nil
 }
